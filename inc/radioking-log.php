72e2572b3b9c2bc7361a53ada28a628764de4b6e
Arthur Violy <arthur@violy.net>
2019-12-08 22:24:59 +0100

    log track

--- inc/radioking-log.php
+++ inc/radioking-log.php
@@ -5,12 +5,12 @@ function radioking_log_track($payload){
 	$success = false;
 
 	$rk_track_id = $payload->id;
-	$wp_track_id = get_track_by_id($rk_track_id);
-	/*
+	$wp_track = get_track_by_id($rk_track_id);
+	$wp_track_id = $wp_track->ID ?? 0;
 	$success = $wpdb->insert($wpdb->prefix.'track_log',[
 		'rk_track_id'=>$rk_track_id,
 		'wp_track_id'=>$wp_track_id,
 	]);
-	*/
-	return ['success'=>$success,'$rk_track_id'=>$rk_track_id,'$wp_track_id'=>$wp_track_id];
+
+	return ['success'=>$success,'wp_track_id'=>$wp_track_id];
 }
