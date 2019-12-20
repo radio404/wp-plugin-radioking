965e3f38fd93bea84669997357a3ef557db676be
Arthur Violy <arthur@violy.net>
2019-12-20 22:47:01 +0100

    import schedules from planning

--- api/radioking-schedules-import.php
+++ api/radioking-schedules-import.php
@@ -11,12 +11,13 @@ function radioking_schedules_import_api_route() {
 function radioking_schedules_import_api_callback() {
 
 	$radioking_access_token = $_POST['radioking_access_token'];
+
 	if ( !$radioking_access_token) {
 		wp_die('sorry you are not allowed to access this data','cheatin eh?',403);
 	}
 
 	try {
-		$schedules = radioking_sync_week_planned($radioking_access_token);
+		$schedules = radioking_sync_week_planned($radioking_access_token, 'import');
 	}catch (Throwable $exception){
 		return new WP_REST_Response(['message'=>$exception->getMessage()], 500);
 	}
--- inc/radioking-core.php
+++ inc/radioking-core.php
@@ -32,7 +32,7 @@ function radioking_get_track_box($access_token=null){
 	return json_decode($response->body)->data;
 }
 
-function radioking_sync_week_planned($access_token=null){
+function radioking_sync_week_planned($access_token=null, $mode='fetch'){
 	$access_token = $access_token ?? radioking_get_token();
 	$api_headers = [ "authorization"=> "Bearer $access_token"];
 	$day = date('w');
@@ -45,21 +45,79 @@ function radioking_sync_week_planned($access_token=null){
 		if($schedule->day_playlist){
 			continue;
 		}
+		$type = 'playlist';
+		$wp_schedule = get_schedule_by_id($schedule->idschedule);
+		$track_listing = get_field('track_listing',$wp_schedule) ?? [];
 
 		if($schedule->idplaylist){
 			$response = Requests::get("https://www.radioking.com/api/playlist/tracks/240028/$schedule->idplaylist?limit=50&offset=0",$api_headers);
 			$schedule->playlist = json_decode($response->body)->data;
 			foreach ($schedule->playlist->tracks as &$track){
+				$wp_track = get_track_by_id($track->idtrack);
 				if($track->idtrackbox === 3){
-					$wp_track = get_track_by_id($track->idtrack);
+					$type = 'podcast';
 					$wp_track->acf = get_fields($wp_track->ID) ?? [];
 					$wp_podcast = get_post($wp_track->acf['album']);
 					$wp_podcast->acf = get_fields($wp_track->acf['album']);
 					$schedule->podcast_id = $wp_track->acf['album'];
 					$schedule->podcast = $wp_podcast;
 					$track->wp_track = $wp_track;
+					$podcast_track_id = $wp_track->ID;
+				}
+				$is_in_track_listing = false;
+				foreach($track_listing as $item){
+					if($wp_track) {
+						$item_track          = $item['track'];
+						$is_in_track_listing |= ( !! $item_track && ( $item_track->ID === $wp_track->ID ) );
+					}else{
+						$is_in_track_listing |= ( !! $item['title'] && ( $item['title'] === $track->title ) );
+					}
+				};
+				if(!$is_in_track_listing){
+					if($wp_track){
+						$track_listing = array_merge($track_listing,[['track'=>$wp_track->ID]]);
+					}else{
+						$track_listing = array_merge($track_listing,[['title'=>$track->title,'tracklength_string'=>$track->tracklength_string,'tr'=>$track]]);
+					}
 				}
 			}
+		}
+
+		$schedule_start = new DateTime($schedule->schedule_start);
+		$schedule_end = new DateTime($schedule->schedule_end);
+		$schedule_interval = $schedule_start->diff($schedule_end,true);
+		$schedule_interval_hms = $schedule_interval->format('%H:%I:%S');
+		$schedule_interval_diff = $schedule_end->getTimestamp() - $schedule_start->getTimestamp();
+		$schedule_start = $schedule_start->format("Y-m-d H:i:s");
+		$schedule_end = $schedule_end->format("Y-m-d H:i:s");
+		$wp_schedule_meta = [
+			'idschedule'=>$schedule->idschedule,
+			'start'=>$schedule_start,
+			'end'=>$schedule_end,
+			'type'=>$type,
+			'duration_seconds'=>$schedule_interval_diff,
+			'duration_literal'=>$schedule_interval_hms,
+			'podcast'=>$schedule->podcast_id??'',
+			'podcast_track'=>$podcast_track_id??'',
+			'track_listing'=>$track_listing
+		];
+
+		$schedule->wp_schedule_meta = $wp_schedule_meta;
+
+		if(!$wp_schedule){
+			$wp_schedule_id = wp_insert_post([
+				'post_title' => $schedule->name,
+				'post_type' => 'schedule',
+				'post_status' => 'publish',
+				'post_name' => sanitize_title("$schedule->name"),
+				'post_date' => $schedule_start,
+				'post_date_gmt' => $schedule_start,
+				//'meta_input' => $wp_track_meta
+			]);
+			$wp_schedule = get_post($wp_schedule_id);
+		}
+		foreach ($wp_schedule_meta as $field_key => $field_value){
+			update_field($field_key, $field_value, $wp_schedule->ID);
 		}
 	}
 	return $radioking_schedules;
